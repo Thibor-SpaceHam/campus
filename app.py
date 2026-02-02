7b958be229534998da58e207b1f43f99b57bf5fa
Thibor <ruben.caballero.lopez@gmail.com>
2026-02-02 13:14:27 +0100

    Terminamos sesión del lunes

--- app.py
+++ app.py
@@ -12,13 +12,17 @@ def login():
     if request.method == "POST":
         usuario = request.form["user"]
         password = request.form["password"]
+        email = request.form["email"]
+        color = request.form["color"]
+
+        """Genera una nueva pagina (template) HTML que muestra el 
+        usuario ingresado, su email y su color favorito.
+        El return condicional actual no servira para nada en este caso."""
 
         print("Usuario ingresado:", usuario)
         print("Password ingresado:", password)
-
-        return f"Usuario {usuario} ha intentado iniciar sesión."
-
-
+        return render_template("user.html", usuario=usuario, email=email, color=color)
+        #return f"<p>Usuario {usuario} ha intentado iniciar sesión.<br><p>Tu correo es: {email}</p><br><p>Tu color favorito es: {color}</p>"
 
     return render_template("login.html")
 
